07f9b90ca759eeef462aa6552e53b8d2988316d2
AndreRRodrigues <83727384+Rayjir@users.noreply.github.com>
2022-11-11 16:48:58 -0300

    first cimmit

---
+++ src/biblioteca/Alunos.java
@@ -0,0 +1,80 @@
+package biblioteca;
+
+public class Alunos {
+    private int matricula;
+    private String nome, endereco, curso, data, multa;
+    
+
+    public Alunos() {
+        super();
+        this.matricula = 0;
+        this.nome = "";
+        this.endereco = "";
+        this.curso = "";
+        this.data = "";
+        this.multa = "";
+
+    }
+
+    public Alunos(int matricula, String nome, String endereco, String curso, String data, String multa) {
+        super();
+        this.matricula = 0;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.curso = curso;
+        this.data = data;
+        this.multa = multa;
+
+    }
+    
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+		this.matricula = matricula;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getcurso() {
+        return curso;
+    }
+
+    public void setSetor(String setor) {
+        this.curso = setor;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+    public String getMulta(){
+        return multa;
+    }
+    public void setMulta(String multa){
+        this.multa = multa;
+    }
+    @Override
+    public String toString() {
+        return  matricula + "," + nome + "," + endereco + "," + curso
+                + "," + data + ","+ multa;
+    }
+}
---
+++ src/biblioteca/ItensEmprestimo.java
@@ -0,0 +1,66 @@
+package biblioteca;
+
+public class ItensEmprestimo{
+    private int codigoitem, codigoemprestimo, codigolivro, codigoperiodico;
+    private String datadevolucao;
+
+    public ItensEmprestimo(){
+        super();
+        this.codigoitem = 0;
+        this.codigoemprestimo = 0;
+        this.codigolivro = 0;
+        this.codigoperiodico = 0;
+        this.datadevolucao = "";
+    }
+
+    public ItensEmprestimo(int codigoitem, int codigoemprestimo, int codigolivro, int codigoperiodico, String datadevolucao){
+        super();
+        this.codigoitem = codigoitem;
+        this.codigoemprestimo = codigoemprestimo;
+        this.codigolivro = codigolivro;
+        this.codigoperiodico = codigoperiodico;
+        this.datadevolucao = datadevolucao;
+    }
+
+    public int getCodigoitem(){ 
+        return codigoitem;
+    }
+
+    public void setCodigoitem(int codigoitem){
+        this.codigoitem = codigoitem;
+    }
+
+    public int getCodigoemprestimo(){
+        return codigoemprestimo;
+    }
+
+    public void setCodigoemprestimo(int codigoemprestimo){
+        this.codigoemprestimo = codigoemprestimo;
+    }
+
+    public int getCodigolivro(){
+        return codigolivro;
+    }
+
+    public void setCodigolivro(int codigolivro){
+        this.codigolivro = codigolivro;
+    }
+
+    public int getCodigoperiodico(){
+        return codigoperiodico;
+    }
+
+    public void setCodigoperiodico(int codigoperiodico){
+        this.codigoperiodico = codigoperiodico;
+    }
+    public String getDatadevolucao(){
+        return datadevolucao;
+    }
+    public void setDatadevolucao(String datadevolucao){
+        this.datadevolucao = datadevolucao;
+    }
+    @Override
+    public String toString(){
+        return codigoitem + "," + codigoemprestimo + "," + codigolivro + "," + codigoperiodico + "," + datadevolucao;
+    }
+}
